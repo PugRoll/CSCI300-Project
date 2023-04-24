4be0091baca94e3a58cb51036717ab41355460eb
PugRoll <phutson02@gmail.com>
2023-04-23 20:35:16 -0400

    more fixed

--- cart.js
+++ cart.js
@@ -1,5 +1,9 @@
+import {createNewBook } from './books.js';
+
+"use strict";
 var cartPrice = 0;
 document.addEventListener("DOMContentLoaded", function loadPrice() {
+    var price = localStorage.getItem("cartPrice")
     var value = document.cookie
         .split("; ");
     var pattern = /[\d]|[\.]/;
@@ -10,6 +14,41 @@ document.addEventListener("DOMContentLoaded", function loadPrice() {
         }
     }
     var p = document.getElementById("CartPrice").textContent = "$".concat(price);
+    console.log("Price of cart on page load: " + price);
+    //Crate an array with the ids
+    var ids = localStorage.getItem("ids").split(",")
+
+    createBookWithId(ids);
     cartPrice = parseFloat(price);
     console.log(price);
 });
+
+
+
+function createBookWithId(ids) {
+    console.log(ids);
+    //JS is weird, just an anonymous function 
+    (async () => {
+        const url = "http://127.0.0.1:8080/books.json"
+        fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            var bookTable = document.getElementById("cart-table");
+            var i = 0;
+            //Loop through collection of books and display only the books that were added to the cart
+            for(const book of data.items) {  
+                for(var i = 0; i < ids.length; i++) {
+                   if(JSON.stringify(book.id).includes(ids[i].toString())) {
+                       console.log(book.title + " added");
+                       bookTable.appendChild(createNewBook(book.title, book.author, book.img_src,
+                                        book.Description, book.price, book.id, true));
+                   } 
+                }
+            }
+
+
+        })
+        .catch(console.error); 
+    })();
+};
+
